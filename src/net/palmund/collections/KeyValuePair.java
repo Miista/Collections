bd556e4be2cedf105a729086e9e8baa762093e35
Søren Palmund <palmund@gmail.com>
2013-11-16 10:00:56 +0100

    Added license.

--- src/net/palmund/collections/KeyValuePair.java
+++ src/net/palmund/collections/KeyValuePair.java
@@ -1,3 +1,10 @@
+/*
+ * Copyright 2013 (c) S¿ren Palmund
+ * 
+ * Licensed under the License described in LICENSE (the "License"); you may not
+ * use this file except in compliance with the License.
+ */
+
 package net.palmund.collections;
 
 public class KeyValuePair<T, V> {
